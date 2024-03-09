9f0ca331ed186b50ac8c63e8f7a39e32e138e906
Soubhagya Srivastava <120361735+shubh-soubhagya@users.noreply.github.com>
2024-03-09 12:57:02 +0530

    Add files via upload

---
+++ JavaBasics/IfElse.java
@@ -0,0 +1,16 @@
+class IfElse{
+    public static void main(String[] args){
+        
+        int a = 10;
+        if(a<10){
+            System.out.println("Less than 10");
+        }    
+        else if(a>10){
+            System.out.println("Greater than 10");
+        }    
+        else {
+            System.out.println("Equal to 10");
+        }
+        
+    }
+}
---
+++ JavaBasics/SwitchCase.java
@@ -0,0 +1,20 @@
+class SwitchCase {
+    public static void main(String[] args){
+        int a = 10;
+        switch(a){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 10:
+                System.out.println("10");
+                break;
+            default:
+                System.out.println("None");
+                
+            
+        }
+    }
+}
