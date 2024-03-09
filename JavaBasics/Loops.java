6d6c0ada74e30166c17926d29b52d479cd76398e
Soubhagya Srivastava <120361735+shubh-soubhagya@users.noreply.github.com>
2024-03-09 12:57:55 +0530

    Add files via upload

---
+++ JavaBasics/Loops.java
@@ -0,0 +1,27 @@
+class Loops{
+    public static void main(String[] args){
+        
+        System.out.println("Do while Loop: ");
+        int i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<10);
+        
+        System.out.println("\n");
+        System.out.println("While Loop:");
+        int j=0;
+        while(j<10){
+            System.out.println(j);
+            j++;
+        }
+        
+        System.out.println("\n");
+        System.out.println("For Loop");
+        for (int k = 0; k<10; k++){
+            System.out.println(k);
+        }
+        
+        
+    }
+}
