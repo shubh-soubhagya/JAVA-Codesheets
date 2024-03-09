2c23f5fd17d4785c999607e952018f46afacdf5c
Soubhagya Srivastava <120361735+shubh-soubhagya@users.noreply.github.com>
2024-03-09 13:01:08 +0530

    Add files via upload

---
+++ Code of Problems/Factorial.java
@@ -0,0 +1,16 @@
+public class Factorial {
+    
+
+public static void main(String[] args){
+
+    int i, fact =1;
+    int number = Integer.parseInt(args[0]);
+    for(i=1;i<=number;i++){
+        fact = fact*i;
+    }
+
+    System.out.println("The factorial of"+ number + "is: " + fact);
+
+
+}
+}
